0d3efce4e96e26e081092de43fc9ef765aca40c6
akakiplimo <adriankiplimo@gmail.com>
2022-01-12 12:45:57 +0000

    insertion sort prototype

--- sort.h
+++ sort.h
@@ -19,11 +19,15 @@ typedef struct listint_s
   struct listint_s *next;
 } listint_t;
 
+/* auxiliary function prototype */
+void swap(listint_t **head, listint_t *node1, listint_t *node2);
+
 /* printing functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
 /* Sorting algorithms */
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 
 #endif
