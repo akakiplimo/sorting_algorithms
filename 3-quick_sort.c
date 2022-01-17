32a82e639dca4eb58659cc9a5a8b30bee5405489
akakiplimo <adriankiplimo@gmail.com>
2022-01-17 09:54:17 +0000

    Quick sort algorithm

---
+++ 3-quick_sort.c
@@ -0,0 +1,74 @@
+#include "sort.h"
+
+
+/**
+* quick_sort - function that sorts an array of integers
+* in ascending order using the Quick sort algorithm
+* @array: array to be sorted
+* @size: size of array
+*/
+void quick_sort(int *array, size_t size)
+{
+if (array == NULL || size <= 1)
+return;
+sort_alg(array, 0, size - 1, size);
+}
+
+/**
+* sort_alg - recursive sorting algorithm
+* @arr: array
+* @left: leftmost index
+* @right: rightmost index
+* @size: size of array
+*/
+void sort_alg(int *arr, int left, int right, size_t size)
+{
+int pivot;
+
+if (left < right)
+{
+pivot = split(arr, left, right, size);
+sort_alg(arr, left, pivot - 1, size);
+sort_alg(arr, pivot + 1, right, size);
+}
+}
+
+/**
+* split - split array
+* @arr: array
+* @left: leftmost index
+* @right: rightmost index
+* @size: size of array
+* Return: pivot index
+*/
+int split(int *arr, int left, int right, size_t size)
+{
+int i, j, pivot, temp;
+
+pivot = arr[right];
+i = left;
+
+for (j = left; j < right; j++)
+{
+if (arr[j] < pivot)
+{
+if (i != j)
+{
+temp = arr[j];
+arr[j] = arr[i];
+arr[i] = temp;
+print_array(arr, size);
+}
+i++;
+}
+}
+if (arr[i] != arr[right])
+{
+temp = arr[i];
+arr[i] = arr[right];
+arr[right] = temp;
+print_array(arr, size);
+}
+
+return (i);
+}
--- sort.h
+++ sort.h
@@ -19,8 +19,10 @@ struct listint_s *prev;
 struct listint_s *next;
 } listint_t;
 
-/* auxiliary function prototype */
+/* auxiliary function prototypes */
 void swap(listint_t **head, listint_t *node1, listint_t *node2);
+void sort_alg(int *arr, int left, int right, size_t size);
+int split(int *arr, int left, int right, size_t size);
 
 /* printing functions */
 void print_array(const int *array, size_t size);
@@ -30,5 +32,6 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif
